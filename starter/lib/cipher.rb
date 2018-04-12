8c21871ec2a6dab6798f23e6366aaef86fff1e54
Alethea Toh <alethea.toh@duke.edu>
2018-04-12 10:04:04 +0800

    done

--- starter/lib/cipher.rb
+++ starter/lib/cipher.rb
@@ -1,2 +1,32 @@
 class Cipher
+  attr_accessor :plain, :cipher
+
+  def initialize
+    @plain = "abcdefghijklmnopqrstuvwxyz"
+    @cipher = "zyxwvutsrqponmlkjihgfedcba"
+  end
+
+# hello -
+  def encode(string)
+    res = ""
+    string.each_char { |ch|
+      orgIdx = (ch.ord) - ('a'.ord)
+      res += @cipher[25 - orgIdx]
+    }
+    puts res
+  end
+
+  def decode(string)
+    res = ""
+    string.each_char { |ch|
+      orgIdx = (ch.ord) - ('a'.ord)
+      res += @plain[orgIdx]
+    }
+    puts res
+  end
+
 end
+
+cipher = Cipher.new
+puts cipher.encode("hello")
+puts cipher.decode("svool")
