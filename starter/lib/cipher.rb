bdbf4c3c86f040509af8af5b8a257a6e3fd3ef23
Ticktock <smu@ticktock.local>
2018-04-12 12:38:07 +0800

    assignment

--- starter/lib/cipher.rb
+++ starter/lib/cipher.rb
@@ -1,2 +1,50 @@
+
+# Plain = "abcdefghijklmnopqrstuvwxyz".split("")
+# Cipherword = "zyxwvutsrqponmlkjihgfedcba".split("")
+
+
+
 class Cipher
+	Plain = "abcdefghijklmnopqrstuvwxyz".split("")
+	Cipherword = "zyxwvutsrqponmlkjihgfedcba".split("")
+	
+	def initialise (codeword)
+		@codeword = codeword
+		puts @codeword
+  	end
+	
+  	def decode (codeword)
+		$newWord = []
+  		codeword_array = codeword.split("")
+  			codeword_array.each do |item|
+	  		# decoded_word = plain[item.index]
+	  		# decoded_word.to_s
+	  		num = Cipherword.index(item)
+	  		result = Plain[num]
+	  		
+	  		$newWord.push(result)
+	  		
+  			end
+  			$newWord = $newWord.join()
+			puts "This is the result for 1: #{$newWord}"
+  		end
+
+
+  	def encode (codeword)
+		$newWord2 = []
+		codeword_array = codeword.split("")
+  			codeword_array.each do |item|
+  				num = Plain.index(item)
+  				result = Cipherword[num]
+  				$newWord2.push(result)
+  			end
+  			$newWord2 = $newWord2.join()
+			print "This is the result for 2 #{$newWord2}"
+  		end
+
 end
+
+svool = Cipher.new
+svool.decode("svool")
+hello = Cipher.new
+hello.encode("hello")
