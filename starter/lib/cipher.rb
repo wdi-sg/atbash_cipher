e4eaec2bea4e10c42903145579fbb238ad6015c5
ChloeLiang <nliangxin@gmail.com>
2018-10-19 10:14:54 +0800

    encode and decode

--- starter/lib/cipher.rb
+++ starter/lib/cipher.rb
@@ -1,2 +1,23 @@
+ALPHABET = ('a'..'z').to_a
+
+# A folding cipher where the mappings are done by "folding the alphabet."
 class Cipher
+  def self.encode(word)
+    result = []
+    word.downcase.each_char do |char|
+      index = ALPHABET.length - 1 - ALPHABET.index(char)
+      result << ALPHABET[index]
+    end
+    result.join
+  end
+
+  def self.decode(word)
+    encode(word)
+  end
 end
+
+puts Cipher.encode('hello')
+puts Cipher.encode('peter')
+
+puts Cipher.decode('svool')
+puts Cipher.decode('kvgvi')
