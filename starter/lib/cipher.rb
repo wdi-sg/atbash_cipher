4058cd6a2b442e8d4ebaa02aad52a6db0da07e0d
damienteo <ksteo89@gmail.com>
2019-01-24 10:30:35 +0800

    complete

--- starter/lib/cipher.rb
+++ starter/lib/cipher.rb
@@ -1,2 +1,44 @@
+plain = "abcdefghijklmnopqrstuvwxyz"
+cipher = "zyxwvutsrqponmlkjihgfedcba"
+
+alphabet = plain.split("")
+
+# code = cipher.split("") 
+code = alphabet.reverse
+# alphabet.reverse
+
 class Cipher
+
+	def encode(alphabet, code, string)
+		split_string = string.split("")
+		translated= []
+		split_string.each do |letter|
+			output = alphabet.find_index(letter)
+			translated << code[output]
+		end
+		translated.join
+	end
+
+	def decode(alphabet, code, string)
+		split_string = string.split("")
+		translated= []
+		split_string.each do |letter|
+			output = code.find_index(letter)
+			translated << alphabet[output]
+		end
+		translated.join
+	end
+
 end
+
+puts "What is the input?"
+
+string=gets.downcase.chomp
+
+encoded = Cipher.new.encode(alphabet, code, string)
+
+puts "encoded:" + encoded
+
+decoded = Cipher.new.decode(alphabet, code, encoded)
+
+puts "decoded:" + decoded
